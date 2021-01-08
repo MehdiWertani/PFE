82e439273401034017697907cb1144b3a12b625f
Houssem Bouali <houssem.bouali@proxym-it.com>
2021-01-08 10:35:35 +0100

    update procedure

--- src/main/resources/procedure_mock.sql
+++ src/main/resources/procedure_mock.sql
@@ -1,12 +1,14 @@
 create
-definer = root@`%` procedure launch_campagne(IN campagneId bigint, IN campagneName varchar(255))
+ procedure launch_campagne(IN campagneId bigint, IN campagneName varchar(255),
+                                                 IN successSms bigint, IN failedSms bigint)
 BEGIN
 
     DECLARE count int;
-    set count = (select count(*) from result_collection); SELECT * FROM campagne_marketings WHERE id = campagneId AND name like campagneName;
+    set count = (select count(*) from result_collection);
+
+    SELECT * FROM campagne_marketings WHERE id = campagneId AND name like campagneName;
 
     set count = count + 1;
     insert into result_collection (id, error_description, execution_status, id_campagne, ko_sms_nbr, succeeded_sms_nbr)
-    VALUES (count, null, 'OK', campagneId, 10, 25);
+    VALUES (count, null, 'OK', campagneId, failedSms, successSms);
 END;
-
